df45ef1a0b492d887c3b196d3c174f056d27a32c
faisal <faisalr@lambdatest.com>
2019-02-13 13:51:49 +0530

    changes for parallel test support

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -2,18 +2,29 @@
 
 var webdriver = require('selenium-webdriver');
 var config_file = '../../conf/' + (process.env.CONFIG_FILE || 'single') + '.conf.js';
-
-var config = require(config_file).config;
-var lt_browsers = null;
-
 if(process.env.LT_BROWSERS) {
-  var lt_browsers = JSON.parse(process.env.LT_BROWSERS);
+  config_file = '../../conf/parallel.conf.js';
 }
 
-console.log(lt_browsers);
+//remove unwanted characters from jenkins json
+var getValidJson = function(jenkinsInput) {
+  var json = process.env.LT_BROWSERS;
+  json = json.replace(/\\n/g, "");
+  json = json.replace('\\/g', '');
+  return json;
+};
+
+var config = require(config_file).config;
+
+var lt_browsers = null;
+if(process.env.LT_BROWSERS) {
+  var jsonInput = getValidJson(process.env.LT_BROWSERS);
+  lt_browsers = JSON.parse(jsonInput);
+}
 
 var createLTSession = function(config, caps){
-  console.log(caps);
+  console.log('capabilities', caps);
+  console.log('selenium address:', config.server);
   return new webdriver.Builder().
     usingServer(config.server).
     withCapabilities(caps).
@@ -23,29 +34,15 @@ var createLTSession = function(config, caps){
 var myHooks = function () {
   this.Before(function (scenario, callback) {
     var world = this;
+    var task_id = parseInt(process.env.TASK_ID || 0);
+    var caps;
     if(lt_browsers) {
-      for (var i = lt_browsers.length - 1; i >= 0; i--) {
-        var common_caps =  {
-          name: "parallel_test",
-          build: "cucumber-js-lambdatest"
-        };
-        if(lt_browsers[i]['lt_browsers'])
-          common_caps['browserName'] = lt_browsers[i]['lt_browsers'];
-        if(lt_browsers[i]['browserVersion'])
-          common_caps['version'] = lt_browsers[i]['browserVersion'];
-        if(lt_browsers[i]['operatingSystem'])
-          common_caps['platform'] = lt_browsers[i]['operatingSystem'];
-        if(lt_browsers[i]['resolution'])
-          common_caps['resolution'] = lt_browsers[i]['resolution'];
-        world.driver = createLTSession(config, common_caps);
-        callback();
-      }
+      caps = getParallelCaps(lt_browsers, task_id);
     } else {
-      var task_id = parseInt(process.env.TASK_ID || 0);
-      var caps = config.capabilities[task_id];
-      world.driver = createLTSession(config, caps);
-      callback();
+      caps = config.capabilities[task_id];
     }
+    world.driver = createLTSession(config, caps);
+    callback();
   });
 
   this.After(function(scenario, callback){
@@ -53,6 +50,34 @@ var myHooks = function () {
       callback();
     });
   });
+};
+
+// Mapping capabilities
+var getParallelCaps = function (lt_browsers, task_id) {
+  var givenCap = lt_browsers[task_id];
+  var returnCap = {name: 'parallel_test', build: 'cucumber-js-lambdatest'};
+  if(givenCap['operatingSystem']) {
+    returnCap['platform'] = givenCap['operatingSystem'];
+  }
+  if(givenCap['browserName']) {
+    returnCap['browserName'] = givenCap['browserName'];
+  }
+  if(givenCap['browserVersion']) {
+    returnCap['version'] = givenCap['browserVersion'];
+  }
+  if(givenCap['resolution']) {
+    returnCap['resolution'] = givenCap['resolution'];
+  }
+  // if(givenCap[]) {
+  //   returnCap['platform'] = givenCap['operatingSystem'];
+  // }
+  // if(givenCap[]) {
+  //   returnCap['platform'] = givenCap['operatingSystem'];
+  // }
+  // if(givenCap[]) {
+  //   returnCap['platform'] = givenCap['operatingSystem'];
+  // }
+  return returnCap;
 };
 
 module.exports = myHooks;
--- scripts/cucumber-runner.js
+++ scripts/cucumber-runner.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+console.log('in runner');
 var child_process = require('child_process');
 var config_file = '../conf/' + (process.env.CONFIG_FILE || 'single') + '.conf.js';
 var config = require(config_file).config;
@@ -6,7 +7,21 @@ var config = require(config_file).config;
 process.argv[0] = 'node';
 process.argv[1] = './node_modules/.bin/cucumber-js';
 
-for(var i in config.capabilities){
+var getValidJson = function(jenkinsInput) {
+	var json = process.env.LT_BROWSERS;
+	json = json.replace(/\\n/g, "");
+	json = json.replace('\\/g', '');
+	return json;
+};
+
+var lt_browsers = null;
+if(process.env.LT_BROWSERS) {
+	console.log(process.env.LT_BROWSERS);
+	var jsonInput = getValidJson(process.env.LT_BROWSERS);
+  	lt_browsers = JSON.parse(jsonInput);
+}
+
+for( var i in (lt_browsers || config.capabilities) ){
   var env = Object.create( process.env );
   env.TASK_ID = i.toString();
   var p = child_process.spawn('/usr/bin/env', process.argv, { env: env } );  
