0f4e424a79f22b5d0142a548c995a64be06e5565
faisal <faisalr@lambdatest.com>
2019-02-14 14:34:49 +0530

    fixes

--- conf/cred.conf.js
+++ conf/cred.conf.js
@@ -1,4 +1,4 @@
 exports.cred = {
-	username: process.env.LT_USERNAME || 'faisalr',
-	access_key: process.env.LT_ACCESS_KEY || '34ixUwLwZu53NVzs8V95u573CiZIJ7KBMvTyT3BSxC0RCE3TM0'
+	username: process.env.LT_USERNAME || 'YOUR_USERNAME',
+	access_key: process.env.LT_ACCESS_KEY || 'YOUR_ACCESS_KEY'
 }
